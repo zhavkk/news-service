7b2549e75695152c60f33ab80208af1218868a0e
mishanya <zhavkk@gmail.com>
2025-07-11 23:53:11 +0300

    demo handlers

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zhavkk/news-service
 go 1.24.5
 
 require (
+	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.52.8
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jackc/pgx/v5 v5.7.5
@@ -14,7 +15,9 @@ require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
--- src/news/internal/handlers/v1/handlers.go
+++ src/news/internal/handlers/v1/handlers.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"context"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/zhavkk/news-service/src/news/internal/dto"
 )
 
@@ -22,4 +23,102 @@ func NewHandler(newsService NewsService) *NewsHandler {
 	return &NewsHandler{
 		newsService: newsService,
 	}
+}
+
+func (h *NewsHandler) RegisterRoutes(router fiber.Router) {
+	news := router.Group("/news")
+
+	news.Post("/", h.CreateNews)
+	news.Get("/:id", h.GetNewsByID)
+	news.Put("/:id", h.UpdateNews)
+	news.Delete("/:id", h.DeleteNews)
+	news.Get("/", h.ListNews)
+}
+
+func (h *NewsHandler) CreateNews(c *fiber.Ctx) error {
+	ctx := c.Context()
+	var req dto.CreateNewsRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ErrorResponse{
+			Status:  fiber.StatusBadRequest,
+			Message: "Invalid request body",
+			Error:   err.Error(),
+		})
+	}
+
+	resp, err := h.newsService.CreateNews(ctx, req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(resp)
+}
+
+func (h *NewsHandler) UpdateNews(c *fiber.Ctx) error {
+	ctx := c.Context()
+	var req dto.UpdateNewsRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ErrorResponse{
+			Status:  fiber.StatusBadRequest,
+			Message: "Invalid request body",
+			Error:   err.Error(),
+		})
+	}
+
+	id := c.Params("id")
+	req.ID = id
+
+	resp, err := h.newsService.UpdateNews(ctx, req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(resp)
+}
+
+func (h *NewsHandler) GetNewsByID(c *fiber.Ctx) error {
+	ctx := c.Context()
+	id := c.Params("id")
+
+	req := dto.GetNewsByIDRequest{ID: id}
+	resp, err := h.newsService.GetNewsByID(ctx, req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(resp)
+}
+
+func (h *NewsHandler) DeleteNews(c *fiber.Ctx) error {
+	ctx := c.Context()
+	id := c.Params("id")
+
+	req := dto.DeleteNewsRequest{ID: id}
+	resp, err := h.newsService.DeleteNews(ctx, req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(resp)
+}
+func (h *NewsHandler) ListNews(c *fiber.Ctx) error {
+	ctx := c.Context()
+	var req dto.NewsListRequest
+
+	if err := c.QueryParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ErrorResponse{
+			Status:  fiber.StatusBadRequest,
+			Message: "Invalid query parameters",
+			Error:   err.Error(),
+		})
+	}
+
+	resp, err := h.newsService.ListNews(ctx, req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(resp)
 }
